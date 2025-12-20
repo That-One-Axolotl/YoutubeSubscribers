1cca38964c7342df63d358ed2ce80c6d5812363e
That One Axolotl <thatoneaxolotlsharingthings@gmail.com>
2025-12-20 14:59:41 -0800

    Update createImages.js

--- createImages.js
+++ createImages.js
@@ -35,7 +35,7 @@ function createImages(elementsToCreate){
         img.style.transform = 'rotate(' + ((Math.random() * 180) - 90) + 'deg)';
         img.style.mixBlendMode = "multiply";
 
-        img.width = "10%";
+        img.width = "10vw";
         container.appendChild(img)
         console.log(src);
 
